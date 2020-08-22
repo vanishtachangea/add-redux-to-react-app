736a222b07826f002b8cff1cd638d1db8c3d57f2
Vanishta Changea <vanishta.changea@gmail.com>
2020-08-22 13:17:10 +0200

    Redux codes  into different files

--- src/index.js
+++ src/index.js
@@ -1,44 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider} from 'react-redux';
+import { store } from './configure-store';
+import {Container} from './counter/container';
 
 
-const countReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1 ;
-    case "DECREMENT":
-      return state - 1 ;
-    default:
-      return state;
-  }
-}
-let store = createStore(countReducer);
-
-
-const mapStateToProps = state => {
-  return {
-    count: state
-  }
-}
-const mapDispatchToProps  = dispatch =>{
-  return{
-    handleDecrementClick:()=> dispatch({type:'INCREMENT'}),
-    handleIncrementClick:()=> dispatch({type:'DECREMENT'})
-  }
-}
-const Component = ({ count, handleDecrementClick, handleIncrementClick}) =>
-  (
-    <div>
-      <h1>Hello react and redux {count}</h1>
-      <button onClick={handleDecrementClick}>Decrement</button>
-      <button onClick={handleIncrementClick}>Increment</button>
-    </div>
-  );
-
-
-const Container = connect(mapStateToProps,mapDispatchToProps)(Component);
 
 const App = () => (
   <Provider store={store}>
